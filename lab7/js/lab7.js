a5cce1817d47db2626ea3bd4834b3b8eb8157e7b
avy-v <166080455+avy-v@users.noreply.github.com>
2024-05-06 11:26:14 -0700

    Update lab7.js

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -20,9 +20,30 @@ function main() {
 // let's get this party started
 main();
 
-function getNameAndSort() {
-	const userName = window.promt("what is your name?");
-  return userName.split('').sort('').join('');
+//function getNameAndSort() {
+	//const userName = window.promt("what is your name?");
+  //return userName.split('').sort('').join('');
+//}
+
+//console.log(getNameAndSort());
+
+function sortUserName() {
+  //prompt to ask for user's name
+  var userName = window.prompt("Hi. Please tell me your name so can fix it lol.");
+  //reads user's name? 
+  console.log("userName = ", userName);
+  //splits the user's name from string to array
+  var nameArray = userName.split('');
+  console.log("nameArray = ", nameArray);
+  //sorts the user's name which is now an array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort= ", nameArraySort);
+  //array goes back to being a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted= ", nameSorted);
+  return nameSorted;
 }
 
-console.log(getNameAndSort());
+//
+document.writeln("Oh hey, guess what. You know that name you gave me or your name ig. Yeah i fixed it.",
+  sortUserName(), "</br>");
