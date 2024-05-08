b144d83b621af4dceaae9350b96fef2a3bfc3d5b
avy-v <166080455+avy-v@users.noreply.github.com>
2024-05-07 22:31:15 -0700

    Update lab8.js

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -25,23 +25,23 @@ function wompwomp(x) {
   return (x%2==0);
 }
 
-wegotanarraylol = [2016, 28, 33, 10, 21, 98, 1984, 1917, ];
+wegotanarraylol = [2016, 28, 33, 10, 596, 21, 98, 1984, 1917, ];
 console.log("check out this sick array ", wegotanarraylol);
 
-var result = wegotanarraylol.map(wompwomp);
+var result1 = wegotanarraylol.map(wompwomp);
 //shold be [true, true, false, true, true, false, true, true, false]
-console.log("let's go and see if this array is even. The results are in and they are: ", result);
+console.log("let's go and see if this array is even. The results are in and they are: ", result1);
 console.log("kinda reminds me of two truths and a lie lol");
 
-var result = wegotanarraylol.map(function(x) {
+var result2 = wegotanarraylol.map(function(x) {
   return x*2;
 })
 //should be[]
-console.log("Okii now lets see how this cool array looks like if we double it ", result);
+console.log("Okii now lets see how this cool array looks like if we double it ", result2);
 console.log("cool, cool... uhhh.... should we double it or take it lol(nervous laugh)");
 
  // Your map results data
- var mapResults = "Your map results here", wegotanarraylol;
+ var mapResults = "Your map results here";
  
  // Use jQuery to select the element by its ID and set the HTML content
- $("#output").html(mapResults,);
+ $("#output").html(mapResults, result2);
