f8e34afa6f8fd527f472ad3f158c04c68c9f99ef
avy-v <166080455+avy-v@users.noreply.github.com>
2024-05-29 19:45:04 -0700

    worked on lab 13 n inter lab linkae

---
+++ lab13/js/lab13.js
@@ -0,0 +1,36 @@
+// index.js - purpose and description here
+// Author: Avy Vargas
+// Date: 05/29/2024
+
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+//function myFunction(param1, param2) {
+  // some code here
+  // return results;
+//}
+
+for (let i = 1; i <= 200; i++) {
+
+  let str = "";
+  if (i % 3 == 0) {
+    str += "𝐹𝒾𝓏𝓏 ";
+  }
+  if (i % 5 == 0) {
+    str += "𝐵𝓊𝓏𝓏 ";
+  }  
+  if (i % 7 == 0) {
+    str += "𝐵𝑜𝑜𝓂 ";
+  }
+  
+  if (str == "") {
+    console.log(i);
+    $("#output").append("<p class='num'>" + i);
+  } else {
+    console.log(str + '!');
+    $("#output").append("<p class='text'>" + str + '!');
+  }
+  
+}
