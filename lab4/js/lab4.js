c5e8925743c8a8ec17d042318e1a88ff015102b8
avy-v <166080455+avy-v@users.noreply.github.com>
2026-05-07 06:21:06 -0700

    heldcommit
    update?5/7/26

---
+++ lab4/js/lab4.js
@@ -0,0 +1,35 @@
+// index.js - purpose and description here
+// Author: Your Name
+// Date:
+
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
+
+//Board is printed at before every turn
+  //a two by two grid is printed
+  //Pencils are printed on the board
+    //on pencil is place on the center horizontal line
+    //second pencil is placed on the top center of pencil one but is facing vertically on the board
+//user input or "spin" is accepted
+  //user input or "spin" is then used as an event trigger 
+//event charli charli is then triggered
+  //top pencil does a random number of spins befor coming to a stop
+  //stop is the located at one of four quaddrants
+    //quadrant that has the pencil stop on it is then checked for its value
+      //quadrant's value is then confirmed as answer and triggers event answerz
+        //answerz is then displayed on screen along with game reset/restart button
